1806ff74aadab164896f492c7a57709bae763c52
Vladimir Zakharchenko <zvk096@gmail.com>
2026-03-24 22:29:50 +0300

    header, fonts and routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,30 @@
-import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import { shops } from './mock/Cards';
+import { Header } from './components/Header';
 import './index.scss';
-import { cards } from './mock/Cards';
-import type { ShopItem } from './components/CheckboxList/types/CheckboxList';
-import { CheckboxList } from './components/CheckboxList/ui/CheckboxList';
-import { CardItems } from './components/CardItems/ui/CardItems';
-import { Timer } from './components/Timer';
-import { ClassCounter } from './components/ClassCounter';
+import { AccountPage } from './pages/AccountPage';
+import { CartPage } from './pages/CartPage';
+import { CatalogPage } from './pages/CatalogPage';
+import { HomePage } from './pages/HomePage';
 
 const App = () => {
-  //////////////////////////////////////////////////////////////
-  // const arr = [2, 5, 7, 3];
-  // const obj = {
-  //   id: 1,
-  //   name: 'Peter',
-  //   age: 38,
-  // };
-  // useEffect(() => {
-  //   console.log('Object.getPrototypeOf(arr):', Object.getPrototypeOf(arr));
-  //   console.log('Object.getPrototypeOf(obj):', Object.getPrototypeOf(obj));
-  //   // console.log('obj.__proto__:', obj.__proto__);
-
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
-  // // Пример изменения свойства прототипа с последующим cleanup
-  // useEffect(() => {
-  //   const originalFind = Array.prototype.find;
-
-  //   Array.prototype.find = function (...args) {
-  //     console.log('мой find вызвался', args);
-  //     return originalFind.apply(this, args);
-  //   };
-
-  //   console.log(arr.find((x) => x === 5));
-  //   // ниже cleanup (вернули все на место)
-  //   return () => {
-  //     Array.prototype.find = originalFind;
-  //   };
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-  // //////////////////////////////////////////////////////////////
-
-  //////CheckboxList////////
-  const [value, setValue] = useState<ShopItem[]>([]); // выбранные чекбоксы
-  //////////////////////////
+  const date = new Date();
+  const footerDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}г.`;
 
   return (
     <>
-      <div style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
-        <ClassCounter />
-        <Timer />
-      </div>
-      <CheckboxList shops={shops} value={value} onChange={setValue} />
-      <CardItems selectedShops={value} cards={cards} />
-
-      {/* <p>1</p>
-      <div>2</div>
-      <>
-        <div>3</div>
-        <div>4</div>
-      </>
-      <span>5</span>
-      <React.Fragment key="2">
-        <div>6</div>
-      </React.Fragment> */}
+      <Header />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/account" element={<AccountPage />} />
+        </Routes>
+      </main>
+      <footer>
+        <p>{footerDate}</p>
+      </footer>
     </>
   );
 };
