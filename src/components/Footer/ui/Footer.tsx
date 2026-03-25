3d5b12b0a817ee6f1c7132126373f54fa40d0feb
Vladimir Zakharchenko <zvk096@gmail.com>
2026-03-25 16:47:49 +0300

    footer tag fixes

--- src/components/Footer/ui/Footer.tsx
+++ src/components/Footer/ui/Footer.tsx
@@ -5,10 +5,8 @@ import styles from './Footer.module.scss';
 export const Footer: React.FC = () => {
   return (
     <footer className={styles['footer']}>
-      <div>
-        <p>Все товары вымышлены.</p>
-        <p>Все совпадения случайны.</p>
-      </div>
+      <p>Все товары вымышлены.</p>
+      <p>Все совпадения случайны.</p>
     </footer>
   );
 };
